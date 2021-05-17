982d74c65b99a2f46a2b252ed531fe287f68fd26
sukhmansra64 <76924528+sukhmansra64@users.noreply.github.com>
2021-05-17 17:42:38 -0400

    A weather application made in html, using css to improve the layout of the application. Javascript is used to fetch weather information and display it to the application using an API.

---
+++ core.js
@@ -0,0 +1,33 @@
+const form = document.querySelector(".Top-banner form");
+const input = document.querySelector(".Top-banner form input");
+const msg = document.querySelector(".Top-banner form .msg");
+const list = document.querySelector(".ajax-section .cities");
+const apiKey = "1773c4b9bb594383d9d33f868be209fd";
+form.addEventListener("submit",evt => {
+    evt.preventDefault();
+    const inputVal= input.value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+    fetch(url).then(response => response.json()).then(data=>{
+        const {main, name, sys, weather } = data;
+        const icon = `https:////openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+        const li = document.createElement("li");
+        li.classList.add("city");
+        li.innerHTML=`<h2 class="city-name" data-name="${name},${sys.country}">
+                        <span>${name}</span>
+                        <sup>${sys.country}</sup>
+                      </h2>
+                      <div class="city-temp">${Math.round(main["temp"])}<sup>°C</sup>
+                        <figure>
+                            <img class="city-icon" src="${icon}" alt="${weather[0]["main"]}">
+                            <figcaption>${weather[0]["description"]}</figcaption>
+                        </figure>`;
+        list.appendChild(li);
+    }).catch(()=>{
+        msg.textContent="Please enter a valid city.";
+    });
+    msg.textContent="";
+    form.reset();
+    input.focus();
+});
+
+
