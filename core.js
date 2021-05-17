543889579c10bb5df3ca9f071dfd8ee0c30f95ee
sukhmansra64 <76924528+sukhmansra64@users.noreply.github.com>
2021-05-17 18:50:54 -0400

    Updated to get rid of duplicate cities, allowing for similarly named cities in different countries to be added.

--- core.js
+++ core.js
@@ -5,8 +5,32 @@ const list = document.querySelector(".ajax-section .cities");
 const apiKey = "1773c4b9bb594383d9d33f868be209fd";
 form.addEventListener("submit",evt => {
     evt.preventDefault();
-    const inputVal= input.value;
+    let inputVal= input.value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+    const listItems = list.querySelectorAll(".ajax-section .city");
+    const listItemsArray = Array.from(listItems);
+    if (listItemsArray.length>0){
+        const filteredArray = listItemsArray.filter(el=>{
+            let content="";
+            if (inputVal.includes(",")){
+                if ((inputVal.split(","[1])).length>2){
+                    inputVal = inputVal.split(","[0]);
+                    content =el.querySelector(".city-name span").textContent.toLowerCase();
+                }else{
+                    content = el.querySelector(".city-name").dataset.name.toLowerCase();
+                }
+            }else{
+                content = el.querySelector(".city-name span").textContent.toLowerCase();
+            }
+            return content == inputVal.toLowerCase();
+        });
+        if (filteredArray.length>0){
+            msg.textContent="City already shown, specify area code or choose a different city.";
+            form.reset();
+            input.focus();
+            return;
+        }
+    }
     fetch(url).then(response => response.json()).then(data=>{
         const {main, name, sys, weather } = data;
         const icon = `https:////openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
