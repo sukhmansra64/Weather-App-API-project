b6c5598b2c99ead794ac979d4c420094d070d72f
sukhmansra64 <76924528+sukhmansra64@users.noreply.github.com>
2021-05-28 20:20:08 -0400

    Added a feature which displays the user's current location's current forecast on launch

---
+++ public/currentLocation.js
@@ -0,0 +1,36 @@
+const endpoint = 'http://ip-api.com/json/?fields=status,message,city,countryCode';
+const weatherApiKey="1773c4b9bb594383d9d33f868be209fd"
+let xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        const response = JSON.parse(this.responseText);
+        if(response.status !== 'success') {
+            console.log('query failed: ' + response.message);
+            return
+        }
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${response.city+", "+response.countryCode}&appid=${weatherApiKey}&units=metric`;
+        fetch(url).then(response => response.json()).then(data => {
+            const {main, name, sys, weather} = data;
+            const icon = `https:////openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+            const li = document.createElement("li");
+            li.classList.add("city");
+            li.innerHTML = `<h2 class="city-name" data-name="${name},${sys.country}">
+                        <span>Current Location</span>
+                      </h2>
+                      <div class="city-temp">${Math.round(main["temp"])}<sup>°C</sup>
+                        <figure>
+                            <img class="city-icon" src="${icon}" alt="${weather[0]["main"]}">
+                            <figcaption>${weather[0]["description"]}</figcaption>
+                        </figure>`;
+            list.appendChild(li);
+        }).catch(() => {
+            msg.textContent = "Please enter a valid city.";
+        });
+        msg.textContent = "";
+        form.reset();
+        inp.focus();
+
+    }
+};
+xhr.open('GET', endpoint, true);
+xhr.send();
