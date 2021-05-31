239d1b3f630c7daa107524fb4a56871f9c259d1b
sukhmansra64 <76924528+sukhmansra64@users.noreply.github.com>
2021-05-30 21:29:48 -0400

    Commented what the server does

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//uses nodejs and expressjs to make a local server and push the files onto the server to be accessed by html
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname+'/public'));
